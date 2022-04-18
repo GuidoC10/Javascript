e769132312abec8391dd7248c905da4a7186adb9
Guido Botta <guidodoyle20@gmail.com>
2022-04-18 18:30:04 -0300

    2 entrega final

--- js/cart.js
+++ js/cart.js
@@ -1,98 +1,120 @@
+let arrayVentas = [];
+let valorGlobo = 3000;
+let valorMacarons = 1500;
+let valorPreservadas = 5000;
+let valorMantitas = 4000;
+let stockGlobos = 5;
+let stockMacarons = 5;
+let stockPreservadas = 5;
+let stockMantitas = 5;
 
-let compras = "si";
-let valor1 = 1500;
-let valor2 = 2000;
-let valor3 = 3000;
-let valor4 = 4000;
-let ventaTotal = 0;
-let envioTotal = 0;
-let envio1 = "Bonificado";
-let envio2 = 500;
-let envio3 = 1000;
-let envio4 = 2000;
-let iva = 1.21;
-
-while (compras == "si") {
-    let venta = 0;
-    let producto = prompt("Por favor ingrese el numero de articulo que desea comprar");
-    let cantidad = prompt("Por favor ingrese la cantidad que desea comprar");
-
-    // CONFIRMO QUE NO SELECCIONEN CANCELAR EN EL PROMPT
-    if (producto !== null && cantidad !== null) {
-        // CONFIRMO QUE INGRESEN VALORES VALIDOS DE PRODUCTO Y CANTIDAD
-        if (!isNaN(producto) && producto > 0 && producto <= 4){
-            if (!isNaN(cantidad)) {
-                switch (producto) {
-                    case '1':
-                        venta = valor1 * cantidad;
-                        break;
-                    case '2':
-                        venta = valor2 *cantidad;
-                        break;
-                    case '3':
-                        venta = valor3 * cantidad;
-                        break;
-                    case '4':
-                        venta = valor4 * cantidad;
-                        break;
-                }
-            } else {
-                alert("Por favor ingrese una Cantidad valida");
-            }
-        }
-        else {
-            alert("Por favor ingrese un Articulo valido");
-        }
-
+class Ventas {
+    constructor (producto, valor, cantidad, total) {
+        this.producto = producto;
+        this.valor = valor;
+        this.cantidad = cantidad;
+        this.total = total;
     }
-    compras = prompt("Desea seguir comprando?").toLowerCase();
-    ventaTotal = venta + ventaTotal;
 }
 
-// SACO EL COSTO DEL VALOR DEL ENVIO
-let envio = prompt(`Por favor ingrese la zona donde realizar en el envio
-         Zona Sur = 1
-         Caba = 2
-         Zona Oeste = 3
-         Zona Norte = 4`);
 
-switch (envio) {
-    case "1":
-        envioTotal = envio1;
-        break;
-    case "2":
-        envioTotal = envio2;
-        break;
-    case "3":
-        envioTotal = envio3;
-        break;
-    case "4":
-        envioTotal = envio4;
-        break;
-
-}
-//HAGO UNA FUNCION PARA AGREGAR EL IVA AL TOTAL DE LA COMPRA
-function sumarIva(x) {
-    return x*iva;
+let globo = document.getElementById("globo");
+globo.addEventListener("click", mensajeglobo);
+function mensajeglobo () {
+    let cantidadGlobo = document.getElementById("cantidadGlobo").value
+    let producto1 = "Globo";
+    let valor1 = valorGlobo;
+    let cantidadGlobo1 = cantidadGlobo;
+    let total1 = valor1 * cantidadGlobo1;
+    stockGlobos = stockGlobos - cantidadGlobo1
+    if (stockGlobos < 0) {
+        mensajeSinStock("Globos");
+    } else {
+    arrayVentas.push( new Ventas(producto1, valor1, cantidadGlobo, total1));
+    mensajeCarrito(cantidadGlobo, "Globos");
+    }
 }
 
-let totalIva = sumarIva(ventaTotal);
 
-//DOY LA DEVOLUCION DEL TOTAL DE LA COMPRA Y DEL COSTO DEL ENVIO
-if (envio == 1) {
-    let infoVenta = document.createElement("ul");
-    infoVenta.innerHTML = `<ul>Total compra Iva incluido ${totalIva}</ul>
-                           <ul>Envio: ${envioTotal}</ul>`;
-
-    let finVenta = document.getElementById("ejemploDom");
-    finVenta.innerHTML = `<h2>Muchas gracias por tu compra</h2>`;
-    finVenta.append(infoVenta);
-} else {
-    let infoVenta1 = document.createElement("ul");
-    infoVenta1.innerHTML = `<ul>Total compra IVA incluido: ${totalIva}</ul>
-                            <ul>Envio: ${envioTotal}</ul>`;
-
-    let finVenta1 = document.getElementById("ejemploDom");
-    finVenta1.innerHTML = `<h2 style= "color:#B97375">Muchas gracias por tu compra</h2>`;
-    finVenta1.append(infoVenta1);
+let macarons = document.getElementById("macarons");
+macarons.addEventListener("click", mensajeMacarons);
+function mensajeMacarons () {
+    let cantidadMacarons = document.getElementById("cantidadMacarons").value
+    let producto1 = "Macarons";
+    let valor1 = valorMacarons;
+    let cantidadMacarons1 = cantidadMacarons;
+    let total1 = valor1 * cantidadMacarons1;
+    stockMacarons = stockMacarons - cantidadMacarons1
+    if (stockMacarons < 0) {
+        mensajeSinStock("Macarons");
+    } else {
+    arrayVentas.push( new Ventas(producto1, valor1, cantidadMacarons, total1));
+    mensajeCarrito(cantidadMacarons, "Macarons");
+    }
 }
+
+
+let preservadas = document.getElementById("preservadas");
+preservadas.addEventListener("click", mensajePreservadas);
+function mensajePreservadas () {
+    let cantidadPreservadas = document.getElementById("cantidadPreservadas").value
+    let producto1 = "Preservadas";
+    let valor1 = valorPreservadas;
+    let cantidadPreservadas1 = cantidadPreservadas;
+    let total1 = valor1 * cantidadPreservadas1;
+    stockPreservadas = stockPreservadas - cantidadPreservadas1
+    if (stockPreservadas < 0) {
+        mensajeSinStock("Rosas Preservadas");
+    } else {
+    arrayVentas.push( new Ventas(producto1, valor1, cantidadPreservadas, total1));
+    mensajeCarrito(cantidadPreservadas, "Rosas Preservadas");
+    }
+}
+
+
+let mantitas = document.getElementById("mantitas");
+mantitas.addEventListener("click", mensajeMantitas);
+function mensajeMantitas () {
+    let cantidadMantitas = document.getElementById("cantidadMantitas").value
+    let producto1 = "Mantitas";
+    let valor1 = valorMantitas;
+    let cantidadMantitas1 = cantidadMantitas;
+    let total1 = valor1 * cantidadMantitas1;
+    stockMantitas = stockMantitas - cantidadMantitas1
+    if (stockMantitas < 0) {
+        mensajeSinStock("Mantitas");
+    } else {
+        arrayVentas.push( new Ventas(producto1, valor1, cantidadMantitas, total1));
+        mensajeCarrito(cantidadMantitas, "Mantitas")
+    }
+}
+
+//GENERO UN ARRAY CON EL TOTAL DE CADA VENTA 
+console.log(arrayVentas);
+let arrayJson = JSON.stringify(arrayVentas);
+console.log(arrayJson);
+localStorage.setItem("ArrayDeVentas", arrayJson);
+
+console.log(JSON.parse(localStorage.getItem("ArrayDeVentas")));
+
+//MENSAJE DE FINALIZACION DE COMPRA
+function mensajeCarrito (x,y) {
+    let limpiarCarrito = document.querySelector("#carrito");
+    limpiarCarrito.innerHTML = "";
+    let finalCarrito = document.createElement("div");
+        finalCarrito.className = "claseFormulario";
+        finalCarrito.innerHTML = `Muchas gracias por tu compra de ${x} ${y}, esperemos que lo disfrutes`;
+        limpiarCarrito.appendChild(finalCarrito);
+}
+
+function mensajeSinStock (x) {
+    let limpiarCarrito = document.querySelector("#carrito");
+    limpiarCarrito.innerHTML = "";
+    let finalCarrito = document.createElement("div");
+        finalCarrito.className = "claseFormulario";
+        finalCarrito.innerHTML = `Te pedimos disculpas, por el momento no contamos con la cantidad de ${x} que nos pedis`;
+        finalCarrito.createElement("button");
+        limpiarCarrito.appendChild(finalCarrito);
+    }
+
+
--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 // GENERO UN ARRAY DONDE CARGAR TODOS LOS USUARIOS DE LA PAGINA
 let arrayUsuarios = []; 
-let nombre1 = "";
 
 // CON UNA CLASE Y UNA FUNCION VOY GENERARNDO AUTOMATICAMENTE CADA OBJETO CON LA INFORMACION DE CADA USUARIO NUEVO QUE SE CREA
 class Usuario {
@@ -17,8 +16,15 @@ function cargarUsuarios (arrayUsuarios) {
     let apellido = prompt("Por favor ingrese su apellido");
     let password = prompt("Por favor ingrese su password");
     let mail = prompt("Por favor ingrese su mail");
-    const nuevoUsuario = new Usuario(nombre, apellido, password, mail);
-    arrayUsuarios.push(nuevoUsuario);
+    let pruebaUsuario = arrayUsuarios.findIndex(buscador => buscador.nombre == nombre);
+    let pruebaMail = arrayUsuarios.findIndex(buscador => buscador.mail == mail);
+    if (pruebaUsuario !== -1 || pruebaMail !== -1 && arrayUsuarios.lenght >=1) {
+        alert("El nombre de usuario o mail ya esta registrado");
+    }
+    else {
+        const nuevoUsuario = new Usuario(nombre, apellido, password, mail);
+        arrayUsuarios.push(nuevoUsuario);
+    }
 }
 
 //LAS OPCIONES DE CARGA O RECUPERACION DE DATOS PARA LOS USUARIOS 
@@ -28,7 +34,6 @@ while ( ingreso == "a" || ingreso == "b" || ingreso == "c") {
         case "a":
             cargarUsuarios(arrayUsuarios);
             ingreso = prompt("Por favor ingrese la opcion desdeada\n A - Cargar un nuevo usuario\n B - Recuperar datos\n C - Salir").toLowerCase();
-            console.log(nombre1);
             break;
         break;
             
@@ -47,5 +52,14 @@ while ( ingreso == "a" || ingreso == "b" || ingreso == "c") {
             break;
     }    
 } 
-// MUESTRO EL ARRAY TOTAL DE USUARIOS
-console.log(arrayUsuarios);
+// PASO EL ARRAY DE USUARIOS AL LOCAL STORAGE CON EL JSON Y LO MUESTRO POR CONSOLA PARA CORROBORAR
+
+let usuariosJson = JSON.stringify(arrayUsuarios);
+console.log(usuariosJson);
+localStorage.setItem("USUARIOS", usuariosJson);
+
+//RECUPERO LOS DATOS DEL LOCAL STORAGE Y LES HAGO PARSE PARA PODER VOLVER A OBTENERLO COMO UN ARRAY
+let usuariosParse = JSON.parse(localStorage.getItem("USUARIOS"));
+console.log(usuariosParse);
+
+
